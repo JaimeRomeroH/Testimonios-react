8df6ae30e20eb7c1bb1bd03cfb7be09a9120191a
JaimeRomero <jaime_im@outlook.com>
2022-06-21 18:25:55 -0500

    Se agrego la etiqueta strong a props.nombre y props.empresa

--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -3,11 +3,17 @@ import '../Estilos/Testimonio.css'
 function Testimonio(props){
     return(
       <div className="contenedor-testimonio">
-         <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto de Ana"/>
+         <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto de Ana" />
          <div className="contenedor-texto-testimonio">
-          <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
-          <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
-          <p className="texto-testimonio">"{props.testimonio}"</p>
+          <p className="nombre-testimonio">
+            <strong>{props.nombre}</strong> en {props.pais}
+          </p>
+          <p className="cargo-testimonio">
+            {props.cargo} en <strong>{props.empresa}</strong>
+          </p>
+          <p className="texto-testimonio">
+            "{props.testimonio}"
+          </p>
          </div>
       </div>
     );

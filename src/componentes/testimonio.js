865106067a015631116ece6031775aab55274324
JaimeRomero <jaime_im@outlook.com>
2022-06-21 00:10:27 -0500

    Se crearon los props en el componente Testimonio

--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -1,13 +1,13 @@
 import React from "react";
 import '../Estilos/Testimonio.css'
-function Testimonio(){
+function Testimonio(props){
     return(
       <div className="contenedor-testimonio">
-         <img className="imagen-testimonio" src={require("../imagenes/testimonio-Ana.jpg")} alt="Foto de Ana"/>
+         <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto de Ana"/>
          <div className="contenedor-texto-testimonio">
-          <p className="nombre-testimonio">Ana García</p>
-          <p className="cargo-testimonio">Ingeniera de software</p>
-          <p className="texto-testimonio">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software."</p>
+          <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+          <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+          <p className="texto-testimonio">"{props.testimonio}"</p>
          </div>
       </div>
     );

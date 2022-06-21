5854bf605182fa9c912c3809d30c079b8c13e541
JaimeRomero <jaime_im@outlook.com>
2022-06-20 22:37:01 -0500

    Se modifico el titulo principal

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Testimonio from './componentes/testimonio';
 function App() {
   return (
     <div className="App">
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       <div className="contenedor-principal">
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+       
         <Testimonio />
       </div>  
     </div>

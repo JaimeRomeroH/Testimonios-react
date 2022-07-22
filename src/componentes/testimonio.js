cd3d5666427ff23bc807cf9071988e2153da72f6
JaimeRomero <jaime_im@outlook.com>
2022-07-22 17:33:39 -0500

    Se modifico el atributo alt en el componente Testimonio

--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -3,7 +3,7 @@ import '../Estilos/Testimonio.css'
 function Testimonio(props){
     return(
       <div className="contenedor-testimonio">
-         <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt="Foto de Ana" />
+         <img className="imagen-testimonio" src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} alt={`Foto de ${props.nombre}`} />
          <div className="contenedor-texto-testimonio">
           <p className="nombre-testimonio">
             <strong>{props.nombre}</strong> en {props.pais}
